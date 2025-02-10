66ff648d5f4a0b64c67f126cbb853ccf7783b431
Jose Neil Silagan Jr. <neilsilagan@gmail.com>
2025-02-10 12:54:52 +0800

    Add the assets to the main activity

--- app/src/main/java/com/silagan/tribalquestbayangdinagyang/MainActivity.java
+++ app/src/main/java/com/silagan/tribalquestbayangdinagyang/MainActivity.java
@@ -1,6 +1,7 @@
 package com.silagan.tribalquestbayangdinagyang;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void toStart(View view) {
+    }
+
+    public void toAbout(View view) {
+    }
+
+    public void toExit(View view) {
     }
 }
