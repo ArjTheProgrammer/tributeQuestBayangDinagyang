fff1d76aafce485628442d8a235094e3dd92ff0c
JB <natividadjosephbrian@gmail.com>
2025-02-20 14:49:21 +0800

    Testing modification

--- app/src/main/java/com/silagan/tribalquestbayangdinagyang/CardGame.java
+++ app/src/main/java/com/silagan/tribalquestbayangdinagyang/CardGame.java
@@ -15,7 +15,12 @@ import android.widget.RelativeLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -95,7 +100,13 @@ public class CardGame extends AppCompatActivity implements View.OnClickListener 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_card_game);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         // Initialize all the views
         tv_status = findViewById(R.id.tv_status);
